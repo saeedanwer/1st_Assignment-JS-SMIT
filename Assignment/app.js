2d63180e30d23b3b98620ee163889135a2793451
saeedanwer <60157516+saeedanwer@users.noreply.github.com>
2023-04-07 20:27:00 +0500

    Add files via upload

---
+++ Assignment/app.js
@@ -0,0 +1,108 @@
+// Task -1
+
+// let firsNum = +prompt("Enter first number:");
+// let secNum = +prompt("Enter second number:");
+
+// if (firsNum > secNum) {
+//     console.log("First Number is grater: " + firsNum)
+// }
+// else {
+//     console.log("Second number is grater: " + secNum)
+// }
+
+
+
+// task-2
+
+
+// let userNum = prompt("Enter number for check sign");
+// if(userNum < 0 ){
+//     console.log("The sign is -")
+// }else{
+//     console.log("The sign is +")
+// }
+
+
+
+// task - 3
+
+
+// let firtNum = +prompt("Enter first number:");
+// let secNum = +prompt("Enter second number:");
+// let thirdtNum = +prompt("Enter third number:");
+// let fourthNum = +prompt("Enter fourth number:");
+// let fifthNum = +prompt("Enter fifth number:");
+
+// let largNum = Math.max(firtNum,secNum,thirdtNum,fourthNum,fifthNum);
+// console.log(largNum);
+
+
+
+// task - 4
+
+
+// for (i = 0; i <= 15; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i + " is even")
+//     }
+//     else {
+//         console.log(i + " is odd")
+//     }
+// }
+
+
+
+// task - 5
+
+
+// let marks = +prompt("Enter your marks out of 100:");
+// if (marks <= 60) {
+//     console.log("Grade F")
+// }
+// else if (marks <= 70) {
+//     console.log("Grade D")
+
+// } else if (marks <= 80) {
+//     console.log("Grade C")
+    
+// } else if (marks <= 90) {
+//     console.log("Grade B");
+// }
+
+
+// else if (marks <= 100) {
+//     console.log("Grade A");
+
+// }
+
+
+// task - 6
+
+
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 == 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 == 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+
+
+// task - 7
+
+
+// for (i = 1; i <= 5; i++) {
+//     let row = "";
+
+//     for (j = 1; j <= i; j++) {
+//         row += "* "
+
+//     }
+//     console.log(row);
+//     document.write(row);
+
+// }
